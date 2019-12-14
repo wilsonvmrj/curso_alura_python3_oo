0565f788fe4b14072e770f1e267e02e1be875454
Wilson Magalhães <wilson.magalhaes@gmail.com>
2019-12-14 12:44:53 -0300

    Finalizado capitulo 5

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ from conta import Conta
 conta = Conta(123,"Wilson",500.0,1000.0)
 
 print("Saldo {}".format(conta.saldo))
+
+print(conta.limite)
+print(conta.titular )
+conta.limite = 2000
+print(conta.limite )
--- conta.py
+++ conta.py
@@ -20,16 +20,16 @@ class Conta:
         self.saca(valor)
         destino.deposita(valor)
     
-        
-    def get_saldo(self):
+    @property    
+    def saldo(self):
         return self.__saldo
-    
-    def get_titular(self):
+    @property
+    def titular(self):
         return self.__titular
-    
-    def get_limite(self):
+    @property
+    def limite(self):
         return self.__limite
-    
-    def set_limite(self,limite):
+    @limite.setter
+    def limite(self,limite):
         self.__limite = limite
 
